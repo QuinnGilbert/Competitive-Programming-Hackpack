3c4181bdaf5bcf81a74d52866f77db29ca7baf69
QuinnGilbert <quinngilbert12and3@gmail.com>
2021-03-22 15:34:41 -0400

    Update PrimeFactorization.java

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -1,4 +1,6 @@
-static ArrayList<Integer> PrimeFactorization{
+
+//returns list of primes of n with duplicates showing up multiple times
+static ArrayList<Integer> factor(int n){
 	ArrayList<Integer> result = new ArrayList<Integer>();
 	for(int i=2;i*i<=n;i++){
 		while(n%i==0){
