53c5650745757d8c123ea3b6386e1084d9cca51d
QuinnGilbert <quinngilbert12and3@gmail.com>
2021-03-22 15:29:56 -0400

    Update PrimeFactorization.java

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -1,3 +1,11 @@
-static int test(){
-	return 1;
+static ArrayList<Integer> PrimeFactorization{
+	ArrayList<Integer> result = new ArrayList<Integer>();
+	for(int i=2;i*i<=n;i++){
+		while(n%i==0){
+			result.add(i);
+			n/=i;
+		}
+	}
+	if(n>1) result.add(i);
+	return result;
 }
