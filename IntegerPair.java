b1abe7fc6b05bd9baa1ed21a7623bf0f340c334a
QuinnGilbert <quinngilbert12and3@gmail.com>
2021-03-25 01:51:10 -0400

    Update IntegerPair.java

--- IntegerPair.java
+++ IntegerPair.java
@@ -1,12 +1,12 @@
 
 //Basic sortable pair of ints
-static class Pair implements<Comparable>{
+static class Pair implements Comparable<Pair>{
 	int x,y;
 	Pair(int X,int Y){
 		x=X;
 		y=Y;
 	}
-	public compareTo(Pair b){
+	public int compareTo(Pair b){
 		if(x==b.x)  return Integer.compare(y,b.y);
 		return Integer.compare(x,b.x);
 	}
